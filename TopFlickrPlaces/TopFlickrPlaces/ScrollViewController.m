268b8d5ed23b5a1bb37602f89ffa768df855c37e
Satindar Dhillon <satindar@gmail.com>
2012-07-26 17:55:05 -0700

    display title of photo in iPad

--- TopFlickrPlaces/TopFlickrPlaces/ScrollViewController.m
+++ TopFlickrPlaces/TopFlickrPlaces/ScrollViewController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (nonatomic, strong) UIBarButtonItem *splitViewBarButtonItem;
+@property (weak, nonatomic) IBOutlet UILabel *toolbarTitle;
 @property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
 @end
 
@@ -22,6 +23,7 @@
 @synthesize scrollView = _scrollView;
 @synthesize imageView = _imageView;
 @synthesize splitViewBarButtonItem = _splitViewBarButtonItem;
+@synthesize toolbarTitle = _toolbarTitle;
 @synthesize toolbar = _toolbar;
 
 - (void)awakeFromNib
@@ -92,6 +94,7 @@
                       
 - (void)viewDidUnload
 {
+    [self setToolbarTitle:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -101,26 +104,31 @@
     return self.imageView;
 }
 
+- (void)setupPhotoInView:(UIImage *)image withTitle:(NSString *)title
+{
+    self.toolbarTitle.text = title;
+    self.imageView.image = image;
+    self.scrollView.contentSize = self.imageView.bounds.size;
+    self.imageView.frame = CGRectMake(0, 0, self.imageView.image.size.width, self.imageView.image.size.height);
+    float heightRatio = self.imageView.bounds.size.height / self.scrollView.bounds.size.height;
+    float widthRatio = self.imageView.bounds.size.width / self.scrollView.bounds.size.width;
+    
+    if (heightRatio > widthRatio) {
+        self.scrollView.zoomScale = 1 / widthRatio;
+    } else {
+        self.scrollView.zoomScale = 1 / heightRatio;
+    }
+}
+
 - (void)fetchPhotoAndSetTitle:(NSDictionary *)photo
 {
-//    self.titleBar.title = [photo valueForKey:FLICKR_PHOTO_TITLE];
-//    self.toolbarTitle.title = [photo valueForKey:FLICKR_PHOTO_TITLE];
     dispatch_queue_t requestQueue = dispatch_queue_create("flickr photo requester", NULL);
     dispatch_async(requestQueue, ^{
         NSData *imageData = [NSData dataWithContentsOfURL:[FlickrFetcher urlForPhoto:photo format:FlickrPhotoFormatLarge]];
         UIImage *image = [UIImage imageWithData:imageData];
+        NSString *title = [photo valueForKey:FLICKR_PHOTO_TITLE];
         dispatch_async(dispatch_get_main_queue(), ^{
-            self.imageView.image = image;
-            self.scrollView.contentSize = self.imageView.bounds.size;
-            self.imageView.frame = CGRectMake(0, 0, self.imageView.image.size.width, self.imageView.image.size.height);
-            float heightRatio = self.imageView.bounds.size.height / self.scrollView.bounds.size.height;
-            float widthRatio = self.imageView.bounds.size.width / self.scrollView.bounds.size.width;
-            
-            if (heightRatio > widthRatio) {
-                self.scrollView.zoomScale = 1 / widthRatio;
-            } else {
-                self.scrollView.zoomScale = 1 / heightRatio;
-            }
+            [self setupPhotoInView:image withTitle:title];
         });
     });
     dispatch_release(requestQueue);
